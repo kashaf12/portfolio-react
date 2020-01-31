272f352bfc87520d7dee694f97ab597f46711239
Kashaf Ahmed <kashafaahmed@gmail.com>
2020-02-01 00:13:31 +0530

    Updates

---
+++ precache-manifest.c3060252ec0b786d24d431fcfe799d42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a80f5c934ad156ca9fe016166f910e1",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "264303572ba2ee103dc5",
+    "url": "/portfolio-react/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9824e0699f0aa4fe6e60",
+    "url": "/portfolio-react/static/css/main.fb5bedd5.chunk.css"
+  },
+  {
+    "revision": "264303572ba2ee103dc5",
+    "url": "/portfolio-react/static/js/2.3b8bfb53.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-react/static/js/2.3b8bfb53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9824e0699f0aa4fe6e60",
+    "url": "/portfolio-react/static/js/main.9dbba5ff.chunk.js"
+  },
+  {
+    "revision": "4355c1b3504c43a9fad0",
+    "url": "/portfolio-react/static/js/runtime-main.53b78f2a.js"
+  },
+  {
+    "revision": "bb038eeaacaf003b65bacad9ac90a511",
+    "url": "/portfolio-react/static/media/1ml.bb038eea.png"
+  },
+  {
+    "revision": "9d6a3232898b62a7190ca9cae2b8122f",
+    "url": "/portfolio-react/static/media/alma.9d6a3232.png"
+  },
+  {
+    "revision": "a03c8c4a6f0ad5d4bce8a46210a835ff",
+    "url": "/portfolio-react/static/media/myImage2.a03c8c4a.png"
+  }
+]);
