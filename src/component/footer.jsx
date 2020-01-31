66109dd1afa0e63fc938f4bfab73b2e7089ad6be
Kashaf Ahmed <kashafaahmed@gmail.com>
2020-01-31 22:34:39 +0530

    commit

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,42 +1,45 @@
 import React from "react";
 
-import codepen from "../img/codepen.png";
+import gmail from "../img/gmail.png";
 import github from "../img/github.png";
 import linkedin from "../img/linkedin.png";
 
 class Footer extends React.Component {
-	render() {
-		return (
-			<div id="footer" className="container-fluid text-center py-5">
-				<div className="container">
-					<div className="row">
-						<div className="col-12 col-sm-12 col-md-8 mx-auto">
-							<a
-								href="https://codepen.io/kodi24fever"
-								target="_blank"
-								rel="noopener noreferrer">
-								<img className="img-fluid" src={codepen} alt=""/>
-							</a>
-							<a
-								className="px-3"
-								href="https://github.com/kodi24fever"
-								target="_blank"
-								rel="noopener noreferrer">
-								<img src={github} alt=""/>
-							</a>
-							<a
-								href="https://www.linkedin.com/in/frankizquierdo"
-								target="_blank"
-								rel="noopener noreferrer">
-								<img src={linkedin} alt=""/>
-							</a>
-						</div>
-					</div>
-					<h5 className="pt-4">Frank Izquierdo &copy; 2018</h5>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="footer" className="container-fluid text-center py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-sm-12 col-md-8 mx-auto">
+              <a
+                href="https://kashafaahmed@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="img-fluid" src={gmail} alt="" />
+              </a>
+              <a
+                className="px-3"
+                href="https://github.com/kashaf12"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={github} alt="" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kashaf-ahmed"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedin} alt="" />
+              </a>
+            </div>
+          </div>
+          <h5 className="pt-4">Kashaf Ahmed &copy; 2020</h5>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
